cbaec704ab9c770ef1386da21af405f3575a9b98
maximeroos <maxime.roos67@gmail.com>
2015-04-24 18:54:56 +0200

    Fini les exercices 1 a 15

---
+++ ex11.rb
@@ -0,0 +1,15 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+print "What's your name?"
+name = gets.chomp
+print "Where you from?"
+city = gets.chomp
+
+puts "My name is #{name} and I come from #{city}!"
